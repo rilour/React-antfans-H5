3fc47d573877f6f0d4eafb45cb367b450da95936
许云涛 <276958756@qq.com>
2022-10-29 00:24:10 +0800

    update message

--- src/home/message.jsx
+++ src/home/message.jsx
@@ -1,14 +1,12 @@
 import React from "react"
 import {MessageOutline} from 'antd-mobile-icons'
-
-
-
+import { Link } from "react-router-dom"
 
 
 function Message(props) {
     return (
         <>
-            <MessageOutline className="message-icon" />
+            <MessageOutline className="message-icon" onClick={<Link to='/message>' />} />
         </>
     )
 }
